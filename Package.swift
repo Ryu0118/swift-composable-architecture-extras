52214e7552a9ba532e7745d2acb4d75641e16f91
Ryu0118 <87907656+Ryu0118@users.noreply.github.com>
2023-08-21 05:23:46 +0900

    Add TaskResult+VoidSuccess

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-composable-architecture-extras",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ComposableArchitectureExtras",
+            targets: ["ComposableArchitectureExtras"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", exact: "1.1.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ComposableArchitectureExtras",
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+            ]
+        ),
+        .testTarget(
+            name: "ComposableArchitectureExtrasTests",
+            dependencies: ["ComposableArchitectureExtras"]
+        ),
+    ]
+)
---
+++ Tests/ComposableArchitectureExtrasTests/ComposableArchitectureExtrasTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import swift_composable_architecture_extras
+
+final class swift_composable_architecture_extrasTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
